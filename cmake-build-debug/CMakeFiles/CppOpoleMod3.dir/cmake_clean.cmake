d5571a510a8f4c09b9c73e84fc9a7be009926dc8
MikiKru <michal_kruczkowski@o2.pl>
2019-11-30 08:48:35 +0100

    mod3 v1

---
+++ cmake-build-debug/CMakeFiles/CppOpoleMod3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppOpoleMod3.dir/main.cpp.obj"
+  "CppOpoleMod3.exe"
+  "CppOpoleMod3.exe.manifest"
+  "CppOpoleMod3.lib"
+  "CppOpoleMod3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppOpoleMod3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
